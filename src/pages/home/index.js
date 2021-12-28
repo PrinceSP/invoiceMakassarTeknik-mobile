fd26d406c8eba87762d0034a868b1f0cd446e4ea
PrinceSP <princepasombaran@gmail.com>
2021-12-28 17:28:16 +0800

    add home page

---
+++ src/pages/home/index.js
@@ -0,0 +1,35 @@
+import React,{useEffect,useState} from 'react'
+import {Text,View,StyleSheet,ScrollView,Image} from 'react-native'
+import {Header,Gap} from '../../components'
+// import {Atomic} from '../../assets'
+import {getCurrentDate} from '../../config'
+
+const Home = ({navigation})=>{
+
+  return(
+    <View style={container}>
+      <Gap height={15}/>
+      <Header name="Beranda" button={true} navigation={navigation}/>
+      <Gap height={15}/>
+      <ScrollView contentContainerStyle={scrollViewCont}>
+        <View style={{flexDirection:'row'}}>
+          <Text style={headingTitle2}>Hello,</Text>
+          <Text style={headingTitle1}> Prince!</Text>
+          <Image style={{right:6,bottom:2}} source={Atomic}/>
+        </View>
+        <Text style={{fontSize:20,fontFamily:'Poppins-Light',color:'#999'}}>{getCurrentDate()}</Text>
+      </ScrollView>
+    </View>
+  )
+}
+
+const style=StyleSheet.create({
+  container:{ flex: 1,backgroundColor:'#fff'},
+  scrollViewCont:{paddingHorizontal:20,paddingBottom:125},
+  headingTitle1:{fontSize:35,fontFamily:'PlayfairDisplay-Bold',color:'#000'},
+  headingTitle2:{fontSize:35,fontFamily:'PlayfairDisplay-Regular',color:'#000'},
+})
+
+const {container,scrollViewCont,headingTitle1,headingTitle2} = style
+
+export default Home
